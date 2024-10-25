d0308cac706b321c1b16425e90563439602585e1
stellarshenson <konrad.jelen@gmail.com>
2024-10-25 15:09:05 +0000

    initial import

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the @stellars/jupyterlab_sublime_theme extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: '@stellars/jupyterlab_sublime_theme:plugin',
+  description: 'Sublime dark theme tweaked by Stellars',
+  autoStart: true,
+  requires: [IThemeManager],
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    console.log('JupyterLab extension @stellars/jupyterlab_sublime_theme is activated!');
+    const style = '@stellars/jupyterlab_sublime_theme/index.css';
+
+    manager.register({
+      name: 'Stellars Sublime Theme',
+      isLight: false,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  }
+};
+
+export default plugin;
